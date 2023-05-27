cb4393819ac528b84786cb7c56500d00f12a828c
Paulo <paulo_vieira22@hotmail.com>
2023-05-27 17:04:37 -0300

    Teste-Stack

--- chat.js
+++ chat.js
@@ -1,8 +1,7 @@
-let listaMensagens = []
+let listaMensagens = [];
 
-let chatMessages = document.querySelector('#chat-messages')
 let messageCommit = document.querySelector('#message-commit');
-let messageInput = document.querySelector('#message-input');
+
 
 
 const adicionarMensagem = (apelido, mensagem) => {
@@ -10,11 +9,10 @@ const adicionarMensagem = (apelido, mensagem) => {
         "apelido": apelido,
         "mensagem": mensagem
     })
-}
-
+};
 const formatarMensagens = () => {
 
-    let htmlData = ''
+    let htmlData = '';
 
     for (let i in listaMensagens){
         htmlData += `
@@ -27,30 +25,35 @@ const formatarMensagens = () => {
             </span>
         </div>
         `
-        
     }
     return htmlData;
-}
+};
 
 const atualizarHTML = () => {
-    chatMessages.innerHTML = formatarMensagens();
+    document.getElementById('chat-messages').innerHTML = formatarMensagens();
+
+    //chatMessages.innerHTML = formatarMensagens();
     
 };
 
 const commitMessageClickHandler = ( ) => {
-    if (messageInput.value.trim().length === 0){
+    let messageInput = document.querySelector('#message-input');
+
+    if (messageInput.value.trim().length == 0){
         messageInput.focus();
         return;
     }
+    
     let palavras = messageInput.value.split(' ');
     let apelido = palavras.shift(); // Remove a primeira palavra e a retorna
     let mensagem = palavras.join(' '); // Junta as palavras restantes com espaço entre elas
     adicionarMensagem(apelido, mensagem);
-    
-    
     atualizarHTML();
     messageInput.value = '';
 
 }
 
+window.addEventListener('load', function () {
+    atualizarHTML();
+});
 
